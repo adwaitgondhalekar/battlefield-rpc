d8a1f3e87301215bf07f57e08a13c31c22775fc1
Ritwik Basak <ritwikbasak14@gmail.com>
2023-09-19 21:55:26 +0530

    2nd commit

--- game_soldier.py
+++ game_soldier.py
@@ -1,10 +1,12 @@
 import multiprocessing
 import time
 import random
+import os
 
 missile_queue = [] # shared missile queue containing current missile
 soldier_speed_list = []
 battlefield = []
+soldier_position_list = []
 N, M, t, T = None, None, None, None
 
 class Missile:
@@ -44,16 +46,26 @@ def missile_approaching(pos, hit_time, missile_type):
     missile = Missile(pos, hit_time, missile_type)
     missile_queue.append(missile)
 
+def status(soldier_ID): # does not return any value
+    # check bitmap whether soldier was hit
+    return was_hit(soldier_ID, true_flag)
+
 def start_exec():
     global N, M, t, T
-    global soldier_speed_list, battlefield
+    global soldier_speed_list, soldier_position_list, battlefield
     
-    N, M, t, T = sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3]
-    soldier_speed_list = [sys.arg(i + 4) for i in range(M)]
+    N, M, t, T = os.sys.argv[0], os.sys.argv[1], os.sys.argv[2], os.sys.argv[3]
+    soldier_speed_list = [os.sys.arg(i + 4) for i in range(M)]
     battlefield = [[0 for i in range(N)] for j in range(N)]
     
-    temp_set = set()
-    while len(temp_set)
+    # assign random positions to each soldier, ensuring NO 2 soldiers at the same position 
+    while len(soldier_position_list) < M:
+        random.seed(round(time.time()))
+        temp_x = random.randint(0, N - 1)
+        temp_y = random.randint(0, N - 1)
+        if (temp_x, temp_y) not in soldier_position_list:
+            soldier_position_list.add((temp_x, temp_y))
+    del temp_x, temp_y
     
     # elect commander
     random.seed(round(time.time()))
