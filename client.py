6b619292d088bc0eab4e3fe5c81a08d581a42b80
Adwait Gondhalekar <adwaitgondhalekar@gmail.com>
2023-09-24 11:21:52 +0530

    Corrected Code Flow

---
+++ client.py
@@ -0,0 +1,157 @@
+import multiprocessing, grpc, random
+from concurrent import futures
+import create_soldier_pb2
+import create_soldier_pb2_grpc as rpc1
+import missile_approaching_pb2
+import missile_approaching_pb2_grpc as rpc2
+import get_valid_position_pb2
+import get_valid_position_pb2_grpc
+import get_params_client_pb2
+import get_params_client_pb2_grpc
+
+
+take_shelter_lock = multiprocessing.Lock()
+global_missile_count_lock = multiprocessing.Lock()
+missile_queue = multiprocessing.Queue()
+no_of_rpc = 2
+servers = []
+global_missile_count = multiprocessing.Queue()
+global_missile_count.put(0)
+
+static_soldier_count = 
+
+# rpc to server to get a single valid position out of available positions for a soldier
+def valid_position_getter(available_pos):
+    with grpc.insecure_channel('[::]:50051') as channel:
+        stub = get_valid_position_pb2_grpc.Get_Valid_PositionStub(channel)
+
+        available_pos_list = []
+        for position in available_pos:
+            available_pos_list.append(get_valid_position_pb2.available_positions(x_pos = position[0], y_pos = position[1]))
+        response = stub.create_soldiers(available_pos_list.__iter__())
+        return (response.valid_x_pos, response.valid_y_pos)
+
+
+
+def take_shelter(soldier_num, x_pos, y_pos, speed, missile_x_pos, missile_y_pos, missile_capacity):
+
+    missile_impact_grid = [[1 for i in range(N)] for j in range(N)]
+    # marking area of impact of missile
+    start_row = max(0, missile_x_pos - (missile_capacity - 1))
+    end_row = min(N - 1, missile_x_pos + (missile_capacity - 1))
+    start_col = max(0, missile_y_pos - (missile_capacity - 1))
+    end_col = min(N - 1, missile_y_pos + (missile_capacity - 1))
+
+    for i in range(start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            missile_impact_grid[i][j] = 0 # area where missile will impact and soldiers die
+    
+    if missile_impact_grid[x_pos][y_pos] == 0:  #current soldier position not safe
+
+        start_row = max(0, x_pos - (speed - 1))
+        end_row = min(N - 1, x_pos + (speed - 1))
+        start_col = max(0, y_pos - (speed - 1))
+        end_col = min(N - 1, y_pos + (speed - 1))
+
+        available_pos = []
+        for i in range(start_row, end_row + 1):
+            for j in range(start_col, end_col + 1):
+                if missile_impact_grid[i][j] != 0:
+                    available_pos.append((i, j)) # available options for soldier to take shelter
+
+        if len(available_pos) == 0:
+            # soldier cannot save himself and thus gets killed
+            return (-1, -1)
+
+        new_pos_x, new_pos_y = valid_position_getter(available_pos)
+        return (new_pos_x, new_pos_y)
+    
+    else:
+        return (x_pos,y_pos) # soldier is safe
+
+
+def soldier_code(soldier_num, x_pos, y_pos, speed, take_shelter_lock, global_missile_count_lock):
+    local_missile_count = 0
+    print("soldier executing " + str(soldier_num))
+
+    while True:
+        
+        if x_pos == -1 :
+            print("soldier killed, terminating target function " + str(soldier_num))
+            break
+        
+        global_missile_count_lock.aquire()
+        local_copy_global_missile_count = global_missile_count.get()
+        global_missile_count.put(local_copy_global_missile_count)
+        global_missile_count_lock.release()
+
+        if missile_queue.empty() != True and local_copy_global_missile_count - 1 == local_missile_count :
+
+            take_shelter_lock.acquire() # so that only 1 soldier executes take_shelter() at a time
+            local_missile_count += 1
+            missile_x_pos, missile_y_pos, hit_time, missile_type = missile_queue.get()
+            missile_capacity = {"M1" : 1, "M2" : 2, "M3" : 3, "M4" : 4}[missile_type]
+
+            new_position = take_shelter(soldier_num, x_pos, y_pos, speed, missile_x_pos, missile_y_pos, missile_capacity)
+            x_pos, y_pos = new_position
+            take_shelter_lock.release()
+
+        
+    
+def create_servers():
+
+    for i in range(no_of_rpc):
+        servers.append(grpc.server(futures.ThreadPoolExecutor(max_workers=1)))
+
+
+    for i, j in enumerate(rpc_list):
+        obj, port, rpc_name  = j[0], j[1], j[2]
+        rpc_name(obj, servers[i])
+        servers[i].add_insecure_port(port)
+        servers[i].start()
+        
+    print("All client side request servers started")
+
+class Missile_Approaching(rpc2.Missile_ApproachingServicer):
+    def missile_approaching(self, request, context):
+        global_missile_count.put(global_missile_count.get() + 1)
+        missile_queue.put((request.x_pos, request.y_pos, request.hit_time, request.missile_type))
+
+class Create_Soldier(rpc1.Create_SoldierServicer):
+
+    def create_soldiers(self, request_iterator, context):
+
+        for soldier in request_iterator:
+
+            multiprocessing.Process(target=soldier_code, args=(soldier.soldier_number, soldier.x_pos, soldier.y_pos, soldier.speed_capacity, take_shelter_lock, global_missile_count_lock)).start()
+
+        response = create_soldier_pb2.soldier_output(msg="Soldiers Created")
+
+        return response
+
+rpc_list = [(Create_Soldier(), '[::]:40051', rpc1.add_Create_SoldierServicer_to_server), (Missile_Approaching(), '[::]:40052', rpc2.add_Missile_ApproachingServicer_to_server)]
+
+
+if __name__ == '__main__':
+
+    create_servers()
+    with grpc.insecure_channel('[::]:50052') as channel:
+        stub = get_params_client_pb2_grpc.Get_Params_ClientStub(channel)
+        response = stub.get_params_client(get_params_client_pb2.params_requset())
+
+        N = response.N
+        
+    while True:
+        pass
+    
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,222 @@
+import os, random, time, grpc
+import create_soldier_pb2
+import create_soldier_pb2_grpc
+import get_valid_position_pb2
+import get_valid_position_pb2_grpc as rpc1
+import missile_approaching_pb2
+import missile_approaching_pb2_grpc
+import get_params_client_pb2
+import get_params_client_pb2_grpc as rpc2
+
+from concurrent import futures
+
+
+
+
+N, M, t, T, commander_index, missile_x_pos, missile_y_pos, missile_type= None, None, None, None, None, None, None, None
+soldier_speed_list = []
+liveness_list = []
+battlefield = []
+soldier_position_list = []
+missile_type_list = ["M1", "M2", "M3", "M4"]
+
+no_of_rpc = 2
+servers = []
+
+commander_x_pos = soldier_position_list[commander_index][0]
+commander_y_pos = soldier_position_list[commander_index][1]
+
+missile_impact_dict = {"M1" : 1, "M2" : 2, "M3" : 3, "M4" : 4}
+missile_impact_grid = []
+
+class Get_Params_Client(rpc2.Get_Params_ClientServicer):
+    def get_params_client(self, request, context):
+        return get_params_client_pb2.params_response(N = N)
+
+class Get_Valid_Position(rpc1.Get_Valid_PositionServicer):
+    def get_valid_position(self, request_iterator, context):
+        final_x_pos, final_y_pos = -1, -1
+        for positions in request_iterator:
+            x_pos, y_pos = positions[0], positions[1]
+
+            if battlefield[x_pos][y_pos] in [1, 3] :
+                continue
+            else:
+                final_x_pos, final_y_pos = x_pos, y_pos
+                break
+        
+        response = get_valid_position_pb2.valid_position(valid_x_pos = final_x_pos, valid_y_pos = final_y_pos)
+
+        return response  # commander returning a valid position where soldier can take shelter
+        
+
+rpc_list = [(Get_Valid_Position(), '[::]:50051',rpc1.add_Get_Valid_PositionServicer_to_server), (Get_Params_Client(), '[::]:50052', rpc2.add_Get_Params_ClientServicer_to_server)]
+
+
+def create_servers():
+
+    for i in range(no_of_rpc):
+        servers.append(grpc.server(futures.ThreadPoolExecutor(max_workers=1)))
+
+
+    for i, j in enumerate(rpc_list):
+        obj, port, rpc_name  = j[0], j[1], j[2]
+        rpc_name(obj, servers[i])
+        servers[i].add_insecure_port(port)
+        servers[i].start()
+        
+    print("All server side request servers started")
+
+def take_shelter(x, y, soldier_index):
+
+    global missile_impact_grid, soldier_speed_list, battlefield, liveness_list
+
+    if missile_impact_grid[x][y] == 0:  #current soldier position not safe
+
+        start_row = max(0, x - (soldier_speed_list[soldier_index] - 1))
+        end_row = min(N - 1, x + (soldier_speed_list[soldier_index] - 1))
+        start_col = max(0, y - (soldier_speed_list[soldier_index] - 1))
+        end_col = min(N - 1, y + (soldier_speed_list[soldier_index] - 1))
+
+        available_pos = []
+        for i in range(start_row, end_row + 1):
+            for j in range(start_col, end_col + 1):
+                if missile_impact_grid[i][j] != 0 and battlefield[i][j] != 1 :
+                    available_pos.append((i, j)) # available options for soldier to take shelter
+
+        if len(available_pos) == 0:
+            # soldier cannot save himself and thus gets killed
+            battlefield[x][y] = 2
+            liveness_list[soldier_index] = 0 # marked soldier as dead
+            return (-1, -1)
+
+
+        random.seed(round(time.time()))
+        random_shelter = random.randint(0, len(available_pos) - 1)
+        new_pos_x, new_pos_y = available_pos[random_shelter]
+        soldier_position_list[soldier_index] = (new_pos_x, new_pos_y) # assigned new position to soldier
+
+        battlefield[x][y] = 0
+        battlefield[new_pos_x][new_pos_y] = 1 # marked soldier's new position in battlefield
+        return (new_pos_x, new_pos_y)
+    
+    else:
+        return (x,y)
+
+
+
+def create_missile():
+
+    global missile_type, missile_x_pos, missile_y_pos, missile_type_list, missile_impact_grid
+
+    random.seed(round(time.time()))
+    missile_x_pos = random.randint(0, N-1)
+    missile_y_pos = random.randint(0, N-1)
+    missile_type = random.choice(missile_type_list)
+
+    # marking area of impact of missile
+    start_row = max(0, missile_x_pos - (missile_impact_dict[missile_type] - 1))
+    end_row = min(N - 1, missile_x_pos + (missile_impact_dict[missile_type] - 1))
+    start_col = max(0, missile_y_pos - (missile_impact_dict[missile_type] - 1))
+    end_col = min(N - 1, missile_y_pos + (missile_impact_dict[missile_type] - 1))
+
+    missile_impact_grid = [[1 for i in range(N)] for j in range(N)]
+    for i in range(start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            missile_impact_grid[i][j] = 0 # area where missile will impact and soldiers die
+    
+
+def take_input():
+
+    global N, M, t, T, soldier_speed_list
+
+    N, M, t, T = int(os.sys.argv[1]), int(os.sys.argv[2]), int(os.sys.argv[3]), int(os.sys.argv[4])
+    soldier_speed_list = [int(os.sys.argv[i + 5]) for i in range(M)]
+
+    #verifying input
+    try:
+        assert N > 0 and M in range(0, N * N + 1) and t > 0 and T >= t
+        for i in range(M):
+            assert soldier_speed_list[i] in range(0, 4 + 1)
+    except AssertionError:
+        print("WRONG HYPERPARAMETERS !\nRUN PROGRAM AGAIN")
+        #terminate program
+
+
+def assign_initial_state():
+
+    global liveness_list, battlefield, soldier_position_list
+
+    battlefield = [[0 for i in range(N)] for j in range(N)]
+    liveness_list = [1 for i in range(M)]
+    
+    # assign random positions to each soldier, ensuring NO 2 soldiers at the same position 
+    while len(soldier_position_list) < M:
+        random.seed(round(time.time()))
+        temp_x = random.randint(0, N - 1)
+        temp_y = random.randint(0, N - 1)
+        if (temp_x, temp_y) not in soldier_position_list:
+            soldier_position_list.append((temp_x, temp_y))
+    del temp_x, temp_y
+    
+    # marking soldiers position in the battlefield
+    for it in soldier_position_list:
+        battlefield[it[0]][it[1]] = 1 
+    
+
+def elect_commander():
+
+    global commander_index
+
+    print("GOT REQUEST TO ELECT COMMANDER")
+
+    alive_index = []
+    for i in range(len(liveness_list)):
+        if liveness_list[i] == 1:
+            alive_index.append(i)
+    
+    random.seed(round(time.time()))
+    commander_index = random.choice(alive_index)
+    x, y = soldier_position_list[commander_index][0], soldier_position_list[commander_index][1]
+    battlefield[x][y] = 3   # marked commander in the battlefield
+
+def create_soldier_process():
+    with grpc.insecure_channel('[::]:40051') as channel:
+        stub = create_soldier_pb2_grpc.Create_SoldierStub(channel)
+
+        soldier_list = []
+        for i in range(M):
+
+            if i == commander_index:
+                continue
+            else:
+                soldier_list.append(create_soldier_pb2.soldier_details(soldier_number = i, x_pos = soldier_position_list[i][0], y_pos = soldier_position_list[i][1], speed_capacity = soldier_speed_list[i]))
+        response = stub.create_soldiers(soldier_list.__iter__())
+        print(response.msg)
+
+if __name__ == '__main__' :
+
+    take_input()
+    assign_initial_state()      # initializing the grid with soldier position, commander not elected yet
+    elect_commander()
+    create_soldier_process()
+    create_servers()
+    start_timestamp = time.time()
+    last_missile_timestamp = None
+    while time.time() - start_timestamp <= T:
+        if last_missile_timestamp != None and time.time() - last_missile_timestamp < t:
+            continue
+        create_missile()
+        new_x, new_y = take_shelter(commander_x_pos, commander_y_pos, commander_index) # first commander tries to take shelter
+        
+        # rpc call to missile_approaching
+        with grpc.insecure_channel('[::]:40052') as channel:
+            stub = missile_approaching_pb2_grpc.Missile_ApproachingStub(channel)
+            stub.missile_approaching(missile_approaching_pb2.missile(x_pos = missile_x_pos, y_pos = missile_y_pos, hit_time = round(time.time() - start_timestamp), missile_type = missile_type))
+        
+        if new_x == -1:
+            elect_commander()
+
+
+
+
